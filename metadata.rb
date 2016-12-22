a59b989df101f87477b1b0d2075d2c720fd206c6
Aleksey Dzyubenko <zzzdzuba@gmail.com>
2016-12-22 16:47:51 +0200

    Update metadata and cookbook depend

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,11 @@
-name 'chef_test_5'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+name 'chef_task_5'
+maintainer 'Alex Dzyubenko'
+maintainer_email 'zzzdzuba@gmail.com'
 license 'all_rights'
-description 'Installs/Configures chef_test_5'
-long_description 'Installs/Configures chef_test_5'
+description 'Installs/Configures chef_task_5'
+long_description 'Installs/Configures chef_task_5'
 version '0.1.0'
+issues_url 'https://github.com/DZUBA/chef_test_5'
+source_url 'https://github.com/DZUBA/chef_test_5'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/chef_test_5/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/chef_test_5' if respond_to?(:source_url)
+depends 'windows', '~> 2.1.1'
