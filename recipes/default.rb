9cd96d6d1c43b923743cf95ef81f6983905c97dc
DZUBA <zzzdzuba@gmail.com>
2016-12-22 15:37:36 +0200

    Added GreenShot installation via windows_package

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2016, The Authors, All Rights Reserved.
+
+# Installing GreenShot via windows_package
+windows_package 'GreenShot' do
+  source 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.9.104/Greenshot-INSTALLER-1.2.9.104-RELEASE.exe'
+  checksum '3B9C34EBACD006834D85E95C0F3DA5363285CA90'
+  installer_type :custom
+  action :install
+end
