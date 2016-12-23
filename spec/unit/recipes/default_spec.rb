7dd3bb9a0df4542bb60b253c80bb5deafdd052ca
DZUBA <zzzdzuba@gmail.com>
2016-12-23 14:50:15 +0200

    Added unit test for windows_package

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,14 +7,9 @@
 require 'spec_helper'
 
 describe 'chef_test_5::default' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'windows', version: '2012R2').converge(described_recipe) }
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+  it 'install GreenShot' do
+    expect(chef_run).to install_windows_package('GreenShot')
   end
 end
