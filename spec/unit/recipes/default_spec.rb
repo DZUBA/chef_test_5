f2b23e2c126e9f0376e38aa41e96cc656f413478
DZUBA <zzzdzuba@gmail.com>
2016-12-23 15:10:30 +0200

    Added unit test for windows_feature

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,6 +10,9 @@ describe 'chef_test_5::default' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'windows', version: '2012R2').converge(described_recipe) }
 
   it 'install GreenShot' do
-    expect(chef_run).to install_windows_package('GreenShot')
+    expect(chef_run).to install_windows_package('GreenShot'). with(source: 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.9.104/Greenshot-INSTALLER-1.2.9.104-RELEASE.exe')
   end
+
+  it '' do
+    expect(chef_run).to install_windows_feature('Hyper-V-PowerShell')
 end
