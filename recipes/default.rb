3c1a0e7415f91d36241351cdc7819fbaa6e0f5b0
DZUBA <zzzdzuba@gmail.com>
2016-12-27 16:11:46 +0200

    Recipe and intagration test refactoring

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,4 +15,5 @@ windows_package 'Greenshot 1.2.9.104' do
   source 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.9.104/Greenshot-INSTALLER-1.2.9.104-RELEASE.exe'
   options '-ms'
   action :install
+  installer_type :inno
 end
