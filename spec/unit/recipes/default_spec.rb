0f70fa5984bc2fdb0e7fe25f7c90e6a432967d8d
DZUBA <zzzdzuba@gmail.com>
2016-12-27 17:33:51 +0200

    Fix unit test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,7 +10,7 @@ describe 'chef_test_5::default' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'windows', version: '2012R2').converge(described_recipe) }
 
   it 'install GreenShot' do
-    expect(chef_run).to install_windows_package('GreenShot'). with(source: 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.9.104/Greenshot-INSTALLER-1.2.9.104-RELEASE.exe')
+    expect(chef_run).to install_windows_package('Greenshot 1.2.9.104'). with(source: 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.9.104/Greenshot-INSTALLER-1.2.9.104-RELEASE.exe')
   end
 
   it 'feature Hyper-V-PowerShell' do
